1618c0fdf04d94426a27cdf2c9588b9c70ba1d9f
VitasDev <58786789+VitasDev@users.noreply.github.com>
2020-02-24 15:55:38 +0100

    Add empty project

---
+++ Cube shoot/Assets/PalyerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PalyerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public GameObject bulletPrefab;
+    // public Camera playerCamera;
+
+    // public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // rb.AddForce(0,0,forwardForce * Time.deltaTime);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bulletObject = Instantiate(bulletPrefab);
+            // bulletObject.transform.position = playerCamera.transform.position + playerCamera.transform.forward;
+            // bulletObject.transform.forward = playerCamera.transform.forward;
+            bulletObject.transform.position = transform.position + transform.forward;
+            bulletObject.transform.forward = transform.forward;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime,0,0);
+        }
+        
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime,0,0);
+        }
+        
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0,0,sidewaysForce * Time.deltaTime);
+        }
+        
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0,0,-sidewaysForce * Time.deltaTime);
+        }
+    }
+}
